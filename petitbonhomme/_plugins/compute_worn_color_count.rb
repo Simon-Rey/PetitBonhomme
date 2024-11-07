7ddb32d25baf7b51d4694b47f5654059866cf87a
Simon-Rey <reysimon@orange.fr>
2024-11-07 22:51:42 +0100

    Doughnut for colors

--- petitbonhomme/_plugins/compute_worn_color_count.rb
+++ petitbonhomme/_plugins/compute_worn_color_count.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 module Jekyll
-  class WardrobeColorCountGenerator < Generator
+  class WornColorCountGenerator < Generator
     safe true
     priority :high
 
@@ -10,10 +10,9 @@ module Jekyll
       clothing_items = YAML.load_file(File.join(site.source, '_data', 'clothing_items.yml'))
       color_definitions = YAML.load_file(File.join(site.source, '_data', 'clothing_items_colors.yml'))
       color_definitions.sort_by! { |color| color['order'] }
+      day_logs = YAML.load_file(File.join(site.source, '_data', 'day_logs.yml'))
 
       # Initialize a hash to store color data by family
-      color_family_counts = {}
-
       color_family_counts = {}
 
       # Create a color lookup hash for quick access and initialize families and colors in color_family_counts
@@ -35,17 +34,32 @@ module Jekyll
         color_family_counts[family_name]['colors'][color['name']] = { 'count' => 0, 'hex' => hex_code }
       end
 
-      # Count occurrences of each color within families
+      # Create a clothing item lookup hash for quick access by item name
+      item_lookup = {}
       clothing_items.each do |item|
-        item['colors'].each do |color_name|
-          if color_lookup.key?(color_name)
-            color_data = color_lookup[color_name]
-            family_name = color_data['family']
-            hex_code = color_data['hex']
+        item_lookup[item['name']] = item
+      end
 
-            # Update family and color counts
-            color_family_counts[family_name]['total'] += 1
-            color_family_counts[family_name]['colors'][color_name]['count'] += 1
+      # Count occurrences of each color in worn items
+      day_logs.each do |log|
+        log['outfits'].each do |outfit|
+          outfit['items'].each do |item_name|
+            # Find the item in clothing items
+            item = item_lookup[item_name]
+            next unless item # Skip if item is not found
+
+            # Update color counts based on item's colors
+            item['colors'].each do |color_name|
+              if color_lookup.key?(color_name)
+                color_data = color_lookup[color_name]
+                family_name = color_data['family']
+                hex_code = color_data['hex']
+
+                # Update family and color counts for worn items
+                color_family_counts[family_name]['total'] += 1
+                color_family_counts[family_name]['colors'][color_name]['count'] += 1
+              end
+            end
           end
         end
       end
@@ -59,7 +73,7 @@ module Jekyll
       end.join(",\n")
 
       # Store formatted JS object string in site data
-      site.data['wardrobe_color_count'] = "[#{ordered_families}]"
+      site.data['worn_color_count'] = "[#{ordered_families}]"
     end
   end
 end
